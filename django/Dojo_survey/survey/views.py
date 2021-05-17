7a49f5b8bfabc02fc9bd6ab9599b3cc0a86b7efe
Lana Theodory <lana.theodory@hotmail.com>
2021-05-17 21:18:02 +0300

    survey

---
+++ django/Dojo_survey/survey/views.py
@@ -0,0 +1,22 @@
+from django.http import request
+from django.shortcuts import render
+
+def index(requests):
+    return render(requests,'index.html')
+
+def info(requests):
+    
+    name = requests.POST['name']
+    email = requests.POST['email']
+    location = requests.POST['location']
+    language = requests.POST['language']
+    comments = requests.POST['comments']
+
+    data = {
+       " name": name,
+       "email":email,
+       "location" : location,
+       "language": language,
+       "comments":comments
+    }
+    return render(requests, "info.html", data)
