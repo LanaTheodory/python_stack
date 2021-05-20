22e60849a7951800c56e9747a0c72f79548ab57a
Lana Theodory <lana.theodory@hotmail.com>
2021-05-20 15:55:14 +0300

    updates

---
+++ django/django_orm/Users/usersapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect, render
+from .models import users
+
+def index(request):
+
+    dict = {
+        "users" : users.objects.all()
+    }
+
+    
+    return render(request, "index.html" , dict )
+
+def index2(request):
+
+    firstname = request.POST["first_name"]
+    lastname = request.POST["last_name"]
+    email = request.POST["email"]
+    age = request.POST["age"]
+
+
+    users.objects.create(first_name= firstname,
+    last_name= lastname,  email= email, age= age)
+
+
+    return redirect("/")
